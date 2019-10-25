812b86c05f147c8ab170ca7da5d146698270d89a
uccessxiong1997 <1006553843@qq.com>
2019-10-25 16:21:43 +0800

    moban

--- small-demo/zhuangshi-tuodong.vue
+++ small-demo/zhuangshi-tuodong.vue
@@ -9,9 +9,9 @@
                 :ref="`tiezi_${i2}`"
                 class="movezj" 
                 :style="`transform:translate(${item2.x}px,${item2.y}px) ;`"
-                @click.stop="iptArIdx = i2" 
-                @touchstart.stop="czClick" 
-                @touchmove.stop="czYidong">
+                
+                @touchstart.stop="czClick(i2)" 
+                @touchmove.stop="czYidong(i2)">
 
 
                 <img :src="item2.src"/>
@@ -35,7 +35,7 @@
                     require("../assets/image/zs-youling.png"),
                     ],
                 wutai: [],
-                iptArIdx: null,
+                
                 }
             },
         
@@ -57,27 +57,27 @@
                     y   : 0,
                     src : this.zhuangshi[i]
                 })
-                this.iptArIdx = this.wutai.length - 1;  //等于最后一个元素
+                
                 
             },
 
-            czClick(e){
-                if(e.touches.length == 1){
-                    let _iptAr = this.wutai[this.iptArIdx];  //选中第几个元素
-                    let _dom   = this.$refs["tiezi_"+this.iptArIdx][0];// 控制第几个dom
+            czClick(index){
+                if(event.touches.length == 1){
+                    let _iptAr = this.wutai[index];  //选中第几个元素
+                    let _dom   = this.$refs["tiezi_"+index][0];// 控制第几个dom
                     _iptAr.domW = _dom.offsetWidth / 2;   //获取元素宽
                     _iptAr.domH = _dom.offsetHeight / 2;       //获取元素高
                 }
             },
-            czYidong(e){
-                e.preventDefault(); 
-                e.stopPropagation();
+            czYidong(index){
+                event.preventDefault(); 
+                event.stopPropagation();
 
 
-                if(e.touches.length == 1){
-                    let _iptAr = this.wutai[this.iptArIdx];   //移动时的第几个元素
-                    let _dom   = this.$refs["tiezi_"+this.iptArIdx][0];  //移动时控制第几个dom
-                    let _point = e.changedTouches[0];
+                if(event.touches.length == 1){
+                    let _iptAr = this.wutai[index];   //移动时的第几个元素
+                    let _dom   = this.$refs["tiezi_"+index][0];  //移动时控制第几个dom
+                    let _point = event.changedTouches[0];
                     let _rect  = _dom.getBoundingClientRect();  //返回元素的边距
                     
                     _iptAr.x += _point.clientX - _rect.left - _iptAr.domW;  
